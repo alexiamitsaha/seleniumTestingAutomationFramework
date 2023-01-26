c6e52951f7fe4d22d1b8873e172084e4a1254be3
Amit Saha <alexiamitsaha@gmail.com>
2023-01-27 03:58:13 +0600

    the internet

--- src/test/java/com/herokuapp/seleniumTestingAutomationFramework/testCases/AbTestingClass.java
+++ src/test/java/com/herokuapp/seleniumTestingAutomationFramework/testCases/AbTestingClass.java
@@ -3,14 +3,13 @@ package com.herokuapp.seleniumTestingAutomationFramework.testCases;
 import com.herokuapp.seleniumTestingAutomationFramework.basicClasses.BasicClass;
 import com.herokuapp.seleniumTestingAutomationFramework.basicClasses.TheInternet;
 import org.junit.Test;
-import org.openqa.selenium.support.ui.Sleeper;
 import org.testng.Assert;
 import com.herokuapp.seleniumTestingAutomationFramework.pages.AbTestingPage;
 import com.herokuapp.seleniumTestingAutomationFramework.utilities.Data;
-import com.herokuapp.seleniumTestingAutomationFramework.basicClasses.TheInternet;
+//import com.herokuapp.seleniumTestingAutomationFramework.basicClasses.TheInternet;
+//import org.openqa.selenium.support.ui.Sleeper;
 
 public class AbTestingClass extends BasicClass {
-
     @Test
     public void ABTesting(){
         TheInternet TI = new TheInternet(Driver); //using this class in java we have to create new instance
@@ -24,12 +23,43 @@ public class AbTestingClass extends BasicClass {
         sleepTest(2000);
 
         //TestCase 02 : Header Validation
+        if (TI.getAbTesting().isDisplayed())
+        {
+            String abTesting= TI.getAbTesting().getText();
+            Assert.assertEquals(Data.AB_TESTING,abTesting);
+        }
+        else {
+            System.out.println(Data.NEGATIVE_ERROR_MASSAGE);
+        }
+        TI.abTesting.click();
+        sleepTest(2000);
 
+        //Testcase 03: Corresponding page URL Validation
 
+        String link = Driver.getCurrentUrl();
+        System.out.println("Given "+link);
+        Assert.assertEquals(link, Data.BASEURL+Data.AB_Test_LINK);
+        System.out.println("Link is Validate");
+        sleepTest(2000);
 
+        //TestCase 04: Paragraph Validation
 
+        if (ABT.getAbTestingPagesVariation1().isDisplayed())
 
+        {
+            String header1= ABT.getAbTestingPagesVariation1().getText();
+            Assert.assertEquals(Data.AB_TEST_VARIATION1,header1);
+        }
+        else if(ABT.getAbTestControl().isDisplayed()){
+            String header2= ABT.AbTestControl.getText();
+            Assert.assertEquals(Data.AB_TEST_VARIATION1,header2);
+        } else {
+            System.out.println(Data.NEGATIVE_ERROR_MASSAGE);
+        }
 
+        sleepTest(2000);
+        Driver.navigate().back();
+        sleepTest(5000);
 
     }
 }
